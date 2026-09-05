c9b5caa0d63052691047f5a8fce3741dcb96977a
ALZOOL <aymanjamma03@gmail.com>
2026-09-05 22:14:18 +0300

    need to addded security

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,15 @@
 {
     'name':"Employee Equipment",
     'author':'Ayman Jammaa',
-    'category':'',
+    'category':'1',
     'version':'18.0.0.1',
     'depends':['base',
                'hr',
               ],
     'data':[
+        'security/equipment_security.xml',
         'security/ir.model.access.csv',
+        
         'views/equipment_view.xml',
         'views/hr_employee_view.xml',
         'views/base_menu.xml',
