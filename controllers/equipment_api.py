f58088589ca8bbb9d28469d18ff1db8ac4356521
ALZOOL <aymanjamma03@gmail.com>
2026-09-16 21:55:21 +0300

    added get by id

--- controllers/equipment_api.py
+++ controllers/equipment_api.py
@@ -2,7 +2,7 @@ from odoo import http
 from odoo.http import request
 
 class EquipmentApi(http.Controller):
-
+    #get all equipment records
     @http.route(
         '/api/equipment',
         type='http',
@@ -25,4 +25,26 @@ class EquipmentApi(http.Controller):
                 'status': record.status,
                 'employee_id': record.employee_id.id if record.employee_id else None,
             })
+        return request.make_json_response(data)
+
+    @http.route(
+        '/api/equipment/<int:equipment_id>',
+        type='http',
+        auth='public',
+        methods=['GET'],
+        csrf=False
+    )
+    def get_equipment_by_id(self, equipment_id):
+        equipment = request.env['device.equipment'].sudo().browse(equipment_id)
+        if not equipment.exists():
+            return request.make_json_response({'error': 'Equipment not found'}, status=404)
+        data = {
+            'id': equipment.id,
+            'name': equipment.name,
+            'serial_number': equipment.serial_number,
+            'purchase_date': equipment.purchase_date,
+            'price': equipment.price,
+            'status': equipment.status,
+            'employee_id': equipment.employee_id.id if equipment.employee_id else None,
+            }
         return request.make_json_response(data)
