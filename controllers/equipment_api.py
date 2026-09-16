bca05e5a55f177babb586ca3825b917fac45e755
ALZOOL <aymanjamma03@gmail.com>
2026-09-16 21:27:34 +0300

    api done as JASON

--- controllers/equipment_api.py
+++ controllers/equipment_api.py
@@ -6,23 +6,23 @@ class EquipmentApi(http.Controller):
     @http.route(
         '/api/equipment',
         type='http',
-        auth='puplic',
+        auth='public',
         methods=['GET'],
         csrf=False
     )
     def get_equipment(self):
-        equipment = request.env['devices.management'].sudo().search([])
+        equipment = request.env['device.equipment'].sudo().search([])
 
         data = []
 
         for record in equipment:
             data.append({
-                'id': equipment.id,
-                'name': equipment.name,
-                'serial_number': equipment.serial_number,
-                'purchase_date': equipment.purchase_date,
-                'price': equipment.price,
-                'status': equipment.status,
-                'employee_id': equipment.employee_id.id if equipment.employee_id else None,
+                'id': record.id,
+                'name': record.name,
+                'serial_number': record.serial_number,
+                'purchase_date': record.purchase_date,
+                'price': record.price,
+                'status': record.status,
+                'employee_id': record.employee_id.id if record.employee_id else None,
             })
         return request.make_json_response(data)
